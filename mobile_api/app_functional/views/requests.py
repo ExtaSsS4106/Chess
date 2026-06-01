a323ae1e1c2f0c878fc2ff8d9e945df70e9fadba
ExtaSsS4106 <ExtaZzZ123456789@yandex.ru>
2026-06-01 14:42:52 +0300

    feat: app functional, friends and request done (NOT TESTED)

--- mobile_api/app_functional/views/requests.py
+++ mobile_api/app_functional/views/requests.py
@@ -1,1 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import User
+from mobile_api.models import Requests
+
+class Get_requests(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    def get(self, request):
+        user = request.user
+        requests = Requests.objects.filter(user_to=user, status='sended').order_by('-id')
+        response = []
+        if request.exists():
+            status = 'ok'
+            for r in requests:
+                response.append({
+                    "id": r.id,
+                    "user_from":{
+                        "id": r.user_from.id,
+                        "name": r.user_from.username
+                    },
+                    "type": r.type
+                })
+        else:
+            status = 'empty'
+        return Response({"status": status, "data": response}, status=status.HTTP_200_OK)
+class Cancel_request(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    def post(self, request):
+        user = request.user
+        rid = request.data.get('rid')
+        
+        if not rid:
+            return Response({"error": "rid required"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        req = get_object_or_404(Requests, id=rid, user_to = user)
+        req.status = 'canceld'
+        req.save()
+        return Response(status=status.HTTP_201_CREATED)
+        
+    
+class Aproove_request(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    def post(self, request):
+        user = request.user
+        rid = request.data.get('rid')
+        
+        if not rid:
+            return Response({"error": "rid required"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        req = get_object_or_404(Requests, id=rid, user_to = user)
+        req.status = 'aprooved'
+        req.save()
+        return Response(status=status.HTTP_201_CREATED)
