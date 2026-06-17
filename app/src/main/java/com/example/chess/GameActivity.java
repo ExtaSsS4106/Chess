6dace9110f5bf6335d8959b3bbc96d50744c8fcd
ExtaSsS4106 <ExtaZzZ123456789@yandex.ru>
2026-06-17 19:55:41 +0300

    TEST: reconecting

--- app/src/main/java/com/example/chess/GameActivity.java
+++ app/src/main/java/com/example/chess/GameActivity.java
@@ -167,7 +167,7 @@ public class GameActivity extends AppCompatActivity {
                                 case "check":
                                     updateBoardFromJson(desk);
                                     runOnUiThread(() -> {
-                                        Toast.makeText(GameActivity.this, json.optString("message"), Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(GameActivity.this, message, Toast.LENGTH_SHORT).show();
                                     });
                                     break;
                                 case "playing":
